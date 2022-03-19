b1e56135a50f49f696e7c30851ea303986a8af23
Carter Dugan <cdugan2020@gmail.com>
2022-03-19 15:22:25 -0400

    Cleaned up the code a little bit, made things more simple

--- linecounter.py
+++ linecounter.py
@@ -1,5 +1,15 @@
 import os
 
+formats = [
+"makefile",
+"py",
+"c", "h",
+"cpp", "hpp",
+"js",
+"cs",
+"php"
+]
+
 def countFileLines(path):
 	with open(path) as f:
 		lines = f.readlines()
@@ -7,28 +17,21 @@ def countFileLines(path):
 		return len(lines)
 
 def countDirectoryLines(path):
-	formats = ["makefile", "py", "c", "h", "cpp", "hpp"]
 
 	directory = os.listdir(path)
 	total = 0
 	for i in directory:
 		if i.split(".")[-1] in formats:
-			total += countFileLines(path + '/' + i)
-		elif os.path.isdir(path + '/' + i):
-			total += countDirectoryLines(path + '/' + i)
+			total += countFileLines(path + i)
+		elif os.path.isdir(path + i):
+			total += countDirectoryLines(path + i)
 	return total
 
-choice = input("1.) File\n2.) Directory\n>>> ")
-try:
-	choice = int(choice)
-except:
-	choice = -1
+path = input("Enter file path: ")
 
-path = input("Now enter the path\n>>> ")
-
-if(choice == 1):
-	countFileLines(path)
-elif(choice == 2):
-	print("\n\nTotal: {} lines of code".format(countDirectoryLines(path)))
+if(os.path.isdir(path)):
+	if path[-1] != '/':
+		path += '/'
+	countDirectoryLines(path)
 else:
-	print("Invalid option")
+	countFileLines(path)
