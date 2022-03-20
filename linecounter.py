600358e6988b0adde9022729ba47778fc496797f
Carter Dugan <cdugan2020@gmail.com>
2022-03-19 21:32:01 -0400

    Adheres to the python standard a little better... a little

--- linecounter.py
+++ linecounter.py
@@ -1,57 +1,52 @@
 import os
 import sys
 
-formats = [
-"makefile",
-"py",
-"c", "h",
-"cpp", "hpp",
-"js",
-"cs",
-"php"
-]
+# TODO: Add comments 
+# TODO: Convert extensions to lowercase
+# TODO: Catch read permission error
 
-def countFileLines(path):
+with open('extensions.txt', 'r') as f:
+	formats = f.readline()
+
+def count_file_lines(path):
 	if not path.split(".")[-1] in formats:
 		return 0
 	try:
-		with open(path) as f:
+		with open(path, 'r') as f:
 			lines = f.readlines()
 			print(path + ":", len(lines))
 			return len(lines)
 	except:
 		print("ERROR: Cannot find '{}' - Not a file or directory. Use '--help' for more info.".format(path))
-		return 0
+		return -1
 
-def countDirectoryLines(path):
+def count_directory_lines(path):
 	if path[-1] != '/':
 		path += '/'
 	directory = os.listdir(path)
 	total = 0
 	for i in directory:
 		if os.path.isdir(path + i):
-			total += countDirectoryLines(path + i)
+			total += count_directory_lines(path + i)
 		else:
-			total += countFileLines(path + i)
+			num = count_file_lines(path + i)
+			if not num == -1:
+				total += num
 	return total
-
-def main():
-
+	
+if __name__  == "__main__":
 	os.chdir(os.path.expanduser('~'))
 
 	if(len(sys.argv) < 2):
 		path = input("Enter file path: ")
 	elif("--help" in sys.argv):
 		print("Usage: linecounter <PATH>")
-		return
+		quit()
 	else:
 		path = sys.argv[1]
 
 	if(os.path.isdir(path)):
-		print("Total: {}".format(countDirectoryLines(path)))
+		print("Total: {}".format(count_directory_lines(path)))
 	else:
-		if(countFileLines(path) == 0):
+		if(count_file_lines(path) == -1):
 			print("ERROR: Cannot find '{}' - Not a file or directory. Use '--help' for more info.".format(path))
-
-if __name__  == "__main__":
-	main()
